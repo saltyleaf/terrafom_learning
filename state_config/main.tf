02f359638b444b0df8671d4bbfcd1aa6e2e07692
Barry Johnson <barry.johnson@flutteruki.com>
2024-06-14 21:51:01 +0100

    added server side encryption

--- state_config/main.tf
+++ state_config/main.tf
@@ -17,3 +17,14 @@ resource "aws_s3_bucket_versioning" "enabled" {
     status = enabled
   }
 }
+
+# enable server side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id 
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
