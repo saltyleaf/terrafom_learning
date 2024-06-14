42210d0dd03023cd5a9ff8259314f0e59e6cb535
Barry Johnson <barry.johnson@flutteruki.com>
2024-06-14 21:46:34 +0100

    added s3 bucket

--- state_config/main.tf
+++ state_config/main.tf
@@ -1,3 +1,19 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "bjhg28lb_terraform-state"
+
+# Prevent accidental bucket deletion
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = enabled
+  }
+}
